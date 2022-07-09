ea18dce9e33fa15f2b247a9ae4770acba20ea7d3
nrojas <nrojas@netmetrix.cl>
2022-07-09 11:09:46 -0400

    feat : add dato to project card

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,24 +5,41 @@ const props = defineProps({
     required: true,
   },
 });
+const imgURL = "/img/" + props.project.img;
 </script>
 
 <template>
   <q-card class="my-card">
     <q-card-section>
-      <div class="text-h6">{{ project.name }}</div>
-      <div class="text-subtitle2">by John Doe</div>
+      <!-- <div class="text-h6">{{ project.name }}</div> -->
+      <!-- <div class="text-subtitle2">by John Doe</div> -->
+      <q-img :src="imgURL">
+        <div class="absolute-bottom text-h6">
+          {{ project.name }}
+        </div>
+      </q-img>
     </q-card-section>
 
     <q-card-section>
       {{ project.description }}
+      {{ project.img }}
     </q-card-section>
 
     <q-separator dark />
 
     <q-card-actions>
-      <q-btn flat>Action 1</q-btn>
-      <q-btn flat>Action 2</q-btn>
+      <q-btn
+        :href="project.links.github"
+        target="_blank"
+        flat
+        icon="mdi-github"
+      ></q-btn>
+      <q-btn
+        :href="project.links.deployed"
+        target="_blank"
+        flat
+        icon="mdi-web"
+      ></q-btn>
     </q-card-actions>
   </q-card>
 </template>
--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,20 +1,20 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { computed } from "vue";
 import { useProjectStore } from "../stores/project";
 import ProjectCard from "../components/ProjectCard.vue";
 
 const projectStore = useProjectStore();
 projectStore.fetchProjects();
 
-/* onMounted(() => {
-  projectStore.fetchProjects();
-}); */
+const projects = computed(() => {
+  return projectStore.projects;
+});
 </script>
 
 <template>
   <div class="row wrap justify-center">
     <div
-      v-for="project in projectStore.projects"
+      v-for="project in projects"
       :key="project.id"
       class="col-xs-10 col-sm-5 col-md-3 q-ma-sm"
     >
