a88b0e5580c566dc601a49e63db8d78597965f5d
nrojas <nrojas@netmetrix.cl>
2022-07-26 23:57:32 -0400

    feat: update projects page

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,20 +9,32 @@ const imgURL = "/img/" + props.project.img;
 </script>
 
 <template>
-  <q-card class="my-card">
+  <q-card class="my-card" style="height: auto">
     <q-card-section>
       <!-- <div class="text-h6">{{ project.name }}</div> -->
       <!-- <div class="text-subtitle2">by John Doe</div> -->
-      <q-img :src="imgURL">
+      <q-img :src="imgURL" style="height: 245px">
         <div class="absolute-bottom text-h6">
           {{ project.name }}
         </div>
       </q-img>
     </q-card-section>
 
-    <q-card-section>
+    <q-card-section style="height: 60px">
       {{ project.description }}
-      {{ project.img }}
+    </q-card-section>
+
+    <q-card-section>
+      <div class="q-gutter-md">
+        <q-icon
+          v-for="language in project.languagesIcons"
+          :key="language"
+          :name="'mdi-' + language"
+          size="md"
+        >
+          <q-tooltip>{{ language }}</q-tooltip>
+        </q-icon>
+      </div>
     </q-card-section>
 
     <q-separator dark />
